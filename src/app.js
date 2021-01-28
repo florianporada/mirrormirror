e77993f26baad7985c4c4dd2aa6c270a82ce1fd8
Florian Porada <florian.porada@gmail.com>
2021-01-28 13:44:59 +0100

    feat: add rotating mirrors + point light

--- src/app.js
+++ src/app.js
@@ -138,12 +138,18 @@ function init() {
   addLight({
     name: 'light1',
     position: { x: -1, y: 1.5, z: -1.5 },
-    color: 0x8800ff,
+    color: 0x8800ff, // #8800ff
   });
   addLight({
     name: 'light2',
     position: { x: 1, y: 1.5, z: -2.5 },
-    color: 0xff0000,
+    color: 0xff0000, // #ff0000
+  });
+
+  addMovingLight({
+    name: 'moving-light',
+    position: { x: -8, y: 0, z: -1 },
+    color: 0xffbb72, // #ffbb72
   });
 
   // Lensflare
@@ -206,23 +212,34 @@ function render() {
     if (mxr) mxr.update(delta);
   });
 
-  // const mirror = scene.getObjectByName('mirror');
-  // const mirror2 = scene.getObjectByName('mirror2');
+  const mirror = scene.getObjectByName('mirror-parent');
+  if (mirror) mirror.rotation.x += 0.005;
 
-  // mirror.rotation.z = -Math.sin(time) / 3;
-  // mirror2.rotation.y = -Math.cos(time) / 2;
+  const mirror2 = scene.getObjectByName('mirror2-parent');
+  if (mirror2) mirror2.rotation.y += 0.005;
 
-  // console.log(mirror2.rotation.y);
+  const mirror3 = scene.getObjectByName('mirror3-parent');
+  if (mirror3) mirror3.rotation.z -= 0.005;
+
+  const movienLight = scene.getObjectByName('moving-light-parent');
+  if (movienLight) {
+    movienLight.rotation.y += 0.01;
+    movienLight.rotation.x = -Math.cos(delta) / 2;
+  }
 
   renderer.render(scene, camera);
 }
 
 // Objects & Assets
-
 function addMirror({ name, rotation, position, size }) {
   const pos = { ...{ x: 0, y: 0, z: 0 }, ...position };
   const rot = { ...{ x: 0, y: 0, z: 0 }, ...rotation };
   const mirrorSize = { ...{ x: 2.4, y: 4.4 }, ...size };
+
+  // parent for mirror + pivot etc...
+  const parent = new THREE.Object3D();
+  // pivots
+  const pivotPoint = new THREE.Object3D();
 
   const reflector = new Reflector(
     new THREE.PlaneBufferGeometry(mirrorSize.x, mirrorSize.y),
@@ -233,25 +250,38 @@ function addMirror({ name, rotation, position, size }) {
   );
 
   reflector.name = name;
+  parent.name = `${name}-parent`;
 
   reflector.position.set(pos.x, pos.y, pos.z);
   reflector.rotation.set(rot.x, rot.y, rot.z);
-
-  scene.add(reflector);
 
   const frameGeometry = new THREE.BoxBufferGeometry(
     mirrorSize.x + 0.2,
     mirrorSize.y + 0.2,
     0.1,
   );
-  const frameMaterial = new THREE.MeshPhongMaterial();
+  const frameMaterial = new THREE.MeshPhongMaterial({
+    color: 0x2194ce,
+    emissive: 0x3d0a0a,
+    specular: 0x2b2b2b,
+    shininess: 100,
+  });
   const frame = new THREE.Mesh(frameGeometry, frameMaterial);
 
   frame.position.z = -0.07;
   frame.castShadow = true;
   frame.receiveShadow = true;
 
+  // defines point around which the object rotates
+  pivotPoint.rotation.z = 0;
+  pivotPoint.rotation.x = 0;
+  pivotPoint.rotation.y = 0;
+
   reflector.add(frame);
+  pivotPoint.add(reflector);
+  parent.add(pivotPoint);
+
+  scene.add(parent);
 }
 
 function addStage({ name, position } = {}) {
@@ -363,6 +393,42 @@ function addLight({ name, position, color, debug = false }) {
   light.target.position.set(0, 0, -2);
 
   scene.add(light.target);
+
+  if (debug) {
+    const helper = new THREE.CameraHelper(light.shadow.camera);
+    scene.add(helper);
+  }
+}
+
+function addMovingLight({ name, position, color, debug = false }) {
+  const pos = { ...{ x: 0, y: 0, z: 0 }, ...position };
+
+  const light = new THREE.PointLight(color, 10, 10);
+  // parent for mirror + pivot etc...
+  const parent = new THREE.Object3D();
+  // pivots
+  const pivotPoint = new THREE.Object3D();
+
+  // "lightbulb"
+  const geometry = new THREE.SphereGeometry(0.05, 32, 32);
+  const material = new THREE.MeshBasicMaterial({ color: color });
+  const sphere = new THREE.Mesh(geometry, material);
+
+  light.name = name;
+  parent.name = `${name}-parent`;
+  light.position.set(pos.x, pos.y, pos.z);
+  light.castShadow = true;
+
+  // defines point around which the object rotates
+  pivotPoint.rotation.z = 0;
+  pivotPoint.rotation.x = 0;
+  pivotPoint.rotation.y = 0;
+
+  light.add(sphere);
+  pivotPoint.add(light);
+  parent.add(pivotPoint);
+
+  scene.add(parent);
 
   if (debug) {
     const helper = new THREE.CameraHelper(light.shadow.camera);
